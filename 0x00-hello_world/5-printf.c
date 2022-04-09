ddbe2461ab998ab892c2e33672646a4bb557aaed
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-09 07:27:43 -0700

    Add: c source file that prints out a statement to the standard output.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Entry point of the program.
+ *
+ * Should always return with value 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\r");
+	return (0);
+}
