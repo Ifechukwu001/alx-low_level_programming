6cafe83a9334af1cd576639bbcd7d03faea004df
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-10 15:14:23 -0700

    Add: c source file that prints all the lowercase characters.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - outputs a character in the alphabet (lowercase).
+ *
+ * Return: Always 0 if successful.
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i != '{')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
