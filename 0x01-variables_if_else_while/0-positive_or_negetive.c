3d483649d4474dd43a833fcadd07119a0db61f58
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-10 11:38:45 -0700

    Add: c source file that generates a random integer and defines its state.

---
+++ 0x01-variables_if_else_while/0-positive_or_negetive.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - generates a random number and decides if the integer is 
+ * positive, negetive or zero.
+ *
+ * Return - Always return 0 if succesful.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	} else if (n = 0)
+	{
+		printf("%d is zero", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
