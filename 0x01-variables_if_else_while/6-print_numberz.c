0b3899bcbe9097d2037fb67f49cd3b3a359991c7
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-12 01:55:08 -0700

    Add: c source file to print all digits of base 10 using putchar.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of the base 10.
+ *
+ * Return: Always 0 if successful.
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
