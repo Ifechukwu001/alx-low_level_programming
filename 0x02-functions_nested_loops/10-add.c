b8c0bb6dae9b438da004cb8140df492a1c845564
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-13 15:34:06 -0700

    Add: c source file containing a function that adds two integers.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers.
+ * @a: first operand.
+ * @b: Second operand.
+ *
+ * Return: The sum of the operands.
+ */
+
+int add(int a,int b)
+{
+	return (a + b);
+}
