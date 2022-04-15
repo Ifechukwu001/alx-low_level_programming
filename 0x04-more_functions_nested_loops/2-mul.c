05bac2f190ffb8b0aeb3beb0bb5671bd22509be2
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-15 01:54:07 -0700

    Add: c source file containing a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two operands together.
+ * @a: First operand.
+ * @b: Second operand.
+ * Return: The product of the operands.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
