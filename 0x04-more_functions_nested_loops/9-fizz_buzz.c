198dbf65c5d5d2916472772c2e69993b20eeac4e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-16 14:48:42 -0700

    Add: c source file that does the FizzBuzz test.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - performs the FizzBuzz test.
+ * Return: Always 0 if successful.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
