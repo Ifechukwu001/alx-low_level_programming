5b323332eb24d443a03732d1dda1411558744047
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-16 14:54:43 -0700

    Fix code to add space only if not last iteration.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,16 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+
+		if (i != 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
