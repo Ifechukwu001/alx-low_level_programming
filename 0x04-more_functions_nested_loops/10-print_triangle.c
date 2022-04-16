92e615b214256d78518cb84978727c68ceb381e7
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-16 15:38:54 -0700

    Add: c source file that prints triangles.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_triange - prints a triangle according to size.
+ * @size: Size of the triangle.
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+	int i , j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j != 0; j--)
+			_putchar(' ');
+		for (j = 0; j < i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
