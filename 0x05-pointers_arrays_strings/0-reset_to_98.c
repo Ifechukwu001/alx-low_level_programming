93ea34743072a216bea5962b243d3873d4692d03
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-20 15:15:01 -0700

    Add: c source file containing a function that changes the value of an integer through a pointer.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value a pointer points to, to 98.
+ * @n: the pointer to the address.
+ * Return: Void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
