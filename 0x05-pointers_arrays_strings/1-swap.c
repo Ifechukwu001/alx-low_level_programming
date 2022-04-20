3250757251772a58cf44067c33b0d6148c78dab8
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-20 15:29:26 -0700

    Add: c source file containing a function that swaps two integers.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _putchar(char);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
