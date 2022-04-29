0e5606d0abf66d542b4ba12a7ef7116644c52ceb
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-29 11:01:56 -0700

    Add c source file containing function that computes the factorial of an integer.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - calculates the mathematical factorial of an integer.
+ * @n: Integer to be computed.
+ * Return: Integer Answer.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
