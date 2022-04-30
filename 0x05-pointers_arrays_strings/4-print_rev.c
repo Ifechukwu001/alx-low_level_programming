37967764aeaacc3fdbda0b3a41099d9ee1bb0ba9
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-04-29 17:12:19 -0700

    Add c source file containing function that prints a string backwards.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * check_null - checks for the NULL's address in a string.
+ * @s: String.
+ * Return: A pointer to NULL.
+ */
+char *check_null(char *s)
+{
+	return ((*s == '\0') ? s : check_null(++s));
+}
+
+/**
+ * print_rev - prints out a string backwards to the output stream.
+ * @s: String to be printed out.
+ * Return: Void.
+ */
+void print_rev(char *s)
+{
+	char *pt = check_null(s);
+
+	while (!(pt < s))
+	{
+		_putchar(*pt);
+		pt--;
+	}
+	_putchar('\n');
+}
