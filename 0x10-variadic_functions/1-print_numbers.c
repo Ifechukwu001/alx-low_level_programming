b3e8d73c08b6757de44571e3595cdb58a39d397f
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-16 15:11:00 -0700

    Fix logic of code.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,15 +15,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		exit(EXIT_FAILURE);
-
-	va_start(numbers, n);
-	for (i = 0; i < n; i++)
+	else
 	{
-		printf("%d", va_arg(numbers, int));
-		if (i == n - 1)
-			printf("\n");
-		else
-			printf("%s", separator);
+		va_start(numbers, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(numbers, int));
+			if (i == n - 1)
+				printf("\n");
+			else
+				printf("%s", separator);
+		}
+		va_end(numbers);
 	}
-	va_end(numbers);
 }
