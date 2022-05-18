8eb1ce5a0259bc2b4b90a4cd90655fc82f69e020
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-17 19:27:54 -0700

    Fix to accomodate zero arguments.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,9 +21,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		if (i == n - 1)
-			printf("\n");
-		else
+		if (!(i == n - 1))
 			printf("%s", separator);
 	}
+	printf("\n");
 }
