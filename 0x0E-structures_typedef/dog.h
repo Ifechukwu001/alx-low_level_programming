368f89931883ff8471bf3e5fafb035b6e5228fcb
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-18 06:40:37 -0700

    Add c header file that defines a structure dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure of a dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Dog's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
