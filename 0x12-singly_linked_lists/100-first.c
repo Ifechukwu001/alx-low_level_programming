8f5f46f65b1477a013aebea6241edc16b27c15f9
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-25 22:47:01 -0700

    Fix to match betty standards.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * call_first - A function called before the main function.
  * Return: Void.
  */
-void  __attribute__((constructor)) call__first(void)
+void  __attribute__((constructor)) call_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
