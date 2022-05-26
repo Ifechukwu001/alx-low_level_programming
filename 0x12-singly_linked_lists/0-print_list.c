46d46b7359d602e5b8cca681c2edd599d700f371
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-25 22:17:49 -0700

    Fix to accomodate NULL pointer.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,8 +8,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	char *string = h->str;
+	char *string;
 
+	if (h == NULL)
+		return (0);
+
+	string = h->str;
 	if (string)
 		printf("[%d] %s\n", h->len, string);
 	else
