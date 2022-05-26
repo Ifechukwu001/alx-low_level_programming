f0ed34527e0db96074af3cfb31fc3be71b3607be
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-26 08:37:48 -0700

    Add c source file containing function that returns the length of a singly linked list.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * listint_len - Prints all the elements of the list.
+ * @h: Pointer to the head of the list.
+ * Return: Size of list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	if (h->next == NULL)
+		return (1);
+	else
+		return (1 + listint_len(h->next));
+}
