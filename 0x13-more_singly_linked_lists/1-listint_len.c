e99e4a94160fc167cb40b11d8e46dcaf9c88e71a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-26 08:44:43 -0700

    Remove global variables.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,9 +7,9 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	if (h == NULL)
+	if (h == (void *)0)
 		return (0);
-	if (h->next == NULL)
+	if (h->next == (void *)0)
 		return (1);
 	else
 		return (1 + listint_len(h->next));
