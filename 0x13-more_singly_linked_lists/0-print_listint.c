6ff8531d0aac7cf02afd1d161ca68128b073f33c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-26 08:26:42 -0700

    Add c source file containing function that prints the values in a singly linked list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of the list.
+ * @h: Pointer to the head of the list.
+ * Return: Number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	printf("%d\n", h->n);
+	if (h->next == NULL)
+		return (1);
+	else
+		return (1 + print_listint(h->next));
+}
