f66d410d47c87b7bded79c9b582884b9b6cbf725
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-05-27 10:07:09 -0700

    Add c source file that pops the head of a list.

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node.
+ * @head: Pointer to the HEAD.
+ * Return: Data value of the popped node.
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *pop, *temp;
+
+	pop = *head;
+	temp = pop->next;
+	*head = temp;
+	n = pop->n;
+	free(pop);
+	if (n)
+		return (n);
+	return (0);
+}
