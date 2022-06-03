37a506c5dddec68162c0199159ba5415206d3428
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-03 15:12:06 -0700

    Change permissions for creating the file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,12 +27,12 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, written __attribute__((unused));
+	int fd, written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0660);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
