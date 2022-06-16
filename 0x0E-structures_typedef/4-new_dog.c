a3c0f5f399e12fa738d4d7d3e3fbfc723bd0102c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-16 23:37:28 +0100

    Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,30 @@
 #include "dog.h"
 
 /**
+ * _strdup - creates a memory array and dumps a string there.
+ * @str: String to be dumped.
+ * Return: Pointer to the memory array.
+ */
+char *_strdup(char *str)
+{
+	int i, str_size = _strlen(str);
+	char *ptr;
+
+	if (str)
+	{
+		ptr = malloc(str_size + 1);
+		if (ptr)
+		{
+			for (i = 0; i < str_size + 1; i++)
+				ptr[i] = str[i];
+		}
+	}
+	else
+		return (0);
+	return (ptr);
+}
+
+/**
  * new_dog - Creates a new dog structure.
  * @name: Dog name.
  * @age: Dog age.
@@ -16,9 +40,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (0);
-	new->name = strdup(name);
+	new->name = _strdup(name);
 	new->age = age;
-	new->owner = strdup(owner);
+	new->owner = _strdup(owner);
 
 	return (new);
 }
