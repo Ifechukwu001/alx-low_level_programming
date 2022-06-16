e1ce926e89bea41e15a0948765008c2efa814ef7
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-16 23:13:57 +0100

    Fix incase any value is missing.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,16 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: %s\n", "(nil)");
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: %f\n", "(nil)");
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", "(nil)");
 }
