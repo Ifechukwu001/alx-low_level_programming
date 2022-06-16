2807fd2ed9811054f3fb2f5701b036922813f26e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-16 23:43:41 +0100

    Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,23 @@
 #include <string.h>
 #include "dog.h"
 
+#include "main.h"
+/**
+ * _strlen - checks the lenght of the string.
+ * @s: Character array (string).
+ * Return: int (the lenght of the string).
+ */
+int _strlen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		++len;
+	}
+	return (len);
+}
+
 /**
  * _strdup - creates a memory array and dumps a string there.
  * @str: String to be dumped.
