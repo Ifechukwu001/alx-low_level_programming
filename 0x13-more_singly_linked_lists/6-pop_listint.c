8e374095c4d5b068c174f83d575b2e121c20304c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-17 06:37:41 +0100

    Fix to accomodate NULL head.

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,15 @@ int pop_listint(listint_t **head)
 	int n;
 	listint_t *pop, *temp;
 
-	if (!(*head == NULL))
-	{
-		pop = *head;
-		temp = pop->next;
-		*head = temp;
-		n = pop->n;
-		free(pop);
-	}
+	if (*head == NULL)
+		return (0);
+	
+	pop = *head;
+	temp = pop->next;
+	*head = temp;
+	n = pop->n;
+	free(pop);
+	
 	if (n)
 		return (n);
 	return (0);
