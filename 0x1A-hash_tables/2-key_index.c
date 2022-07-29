52904f4eb1f590f05b1a502175160acaadc33931
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-29 14:18:02 +0100

    C source file containing function that generates the index of a key in the hash table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Generates the index to save the key/value pair in the hash table.
+ * @key: Key used to generate the index.
+ * @size: Size of the hash table.
+ * Return: Index to save the data.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
