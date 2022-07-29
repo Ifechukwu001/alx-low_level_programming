1300aa6fa1642ee717bf471e667380beb2d98f1c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-29 14:21:30 +0100

    C source file containing function that prints all the key-value pairs in a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints all the nodes in a hash table.
+ * @ht: Pointer to the hash table.
+ * Return: Void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int idx;
+	int first_flag = 1;
+
+	putchar('{');
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		current = ht->array[idx];
+		while (current)
+		{
+			if (first_flag)
+				first_flag = 0;
+			else
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+		}
+	}
+	
+	printf("}\n");
+}
