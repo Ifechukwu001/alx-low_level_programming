dc7d7ce70951ec9634e85da5503cd6c71246bf60
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-29 14:20:42 +0100

    C source file containing function that gets a value associated with a key in the hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Gets the value associated with a key.
+ * @ht: Pointer to the hash table.
+ * @key: Key to the value.
+ * Return: The Value associated with the key or NULL if unsuccessful.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int idx;
+
+	idx = key_index((unsigned char *)key, ht->size);
+	current = ht->array[idx];
+	while (current)
+	{
+		if ((strcmp(key, current->key)) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
